63b6337cd2ee073883c126105de91b12910f331e
William R. Fraser <wfraser@nimbus.codewise.org>
2011-04-11 09:26:57 -0700

    Whoops, log levels in the wrong order.

--- global.h
+++ global.h
@@ -12,10 +12,10 @@
 #endif
 
 enum {
-    LOG_LEVEL_DEBUG,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARN,
     LOG_LEVEL_ERROR,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG
 };
 
 #ifndef NODEBUG
